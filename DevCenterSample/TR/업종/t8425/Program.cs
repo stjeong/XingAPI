54c77893874d133cb49aaa397a764d45153a406d
SeongTae Jeong <kevin13@chol.net>
2020-02-04 20:53:16 +0900

    t8425 예제 추가

---
+++ DevCenterSample/TR/업종/t8425/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using XingAPINet;
+
+namespace t8425
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool useDemoServer = true;
+            Program pg = new Program();
+
+            if (args.Length == 1 && args[0] == "hts")
+            {
+                useDemoServer = false;
+            }
+
+            pg.Main(useDemoServer);
+        }
+
+        void Main(bool useDemoServer)
+        {
+            LoginInfo user = GetUserInfo(useDemoServer);
+
+            using (XingClient xing = new XingClient(useDemoServer))
+            {
+                if (xing.ConnectWithLogin(user) == false)
+                {
+                    Console.WriteLine(xing.ErrorMessage);
+                    return;
+                }
+
+                if (useDemoServer)
+                {
+                    var items = XQt8425.Get();
+                    foreach (var item in items)
+                    {
+                        item.Dump(Console.Out, DumpOutputType.Inline);
+                    }
+                }
+                else
+                {
+                    using (var query = new XQt8425())
+                    {
+                        if (query.Request() < 0)
+                        {
+                            Console.WriteLine("Failed to send request");
+                        }
+
+                        XQt8425OutBlock[] outBlocks = query.GetBlocks();
+                        foreach (var item in outBlocks)
+                        {
+                            if (item.IsValidData == true)
+                            {
+                                item.Dump(Console.Out, DumpOutputType.Inline);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private LoginInfo GetUserInfo(bool useDemoServer)
+        {
+            string fileName = (useDemoServer == true) ? "ebest.demo.txt" : "ebest.hts.txt";
+            string filePath = $@"d:\settings\{fileName}";
+
+            return LoginInfo.FromEncryptedFile(filePath);
+        }
+    }
+}
